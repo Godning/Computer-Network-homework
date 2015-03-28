1d805f398b26f45c0a2bc76b73b23b08daf25974
Godning <736397792@qq.com>
2015-03-28 22:16:33 +0800

    add c# web Server

---
+++ homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWebServer MWS = new MyWebServer();
+            Console.Read();
+        }
+    }
+}
